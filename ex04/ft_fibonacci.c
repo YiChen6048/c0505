06cf69116b7c3ce6726c35805b1eb28bbecf16eb
Yi Chen Tan <yi-ctan@u82z02s01.42kl.edu.my>
2025-03-17 18:20:07 +0800

    added ex04

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yi-ctan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 17:09:46 by yi-ctan           #+#    #+#             */
+/*   Updated: 2025/03/17 17:27:00 by yi-ctan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+int main()
+{
+	int i;
+	for (i = -1; i <=10; i++)
+		printf("ft_fibonacci(%d) = %d\n", i, ft_fibonacci(i));
+	return (0);
+}
+
